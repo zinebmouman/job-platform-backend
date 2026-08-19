20a991037825afa7eb9f16ba9a6fb7b5641c1f79
Zineb Mouman <moumanzineb9@gmail.com>
2026-08-19 18:07:09 +0100

    Initial  frontend

---
+++ src/main/java/com/back/job_platform_backend/JobPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.back.job_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/job_platform_backend/JobPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.job_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
